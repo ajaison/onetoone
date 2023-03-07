91c718c28edb64425c5a3e8826d8c27f9fa44eb9
Alan Jaison <ajaison61@googlemail.com>
2023-03-07 15:39:47 +0000

    Created a centeralised error handling system
    Signed-off-by: jalan <jalan@vmware.com>

--- src/main/java/com/example/onetoone/EmployeeHttpController.java
+++ src/main/java/com/example/onetoone/EmployeeHttpController.java
@@ -1,11 +1,14 @@
 package com.example.onetoone;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
@@ -29,10 +32,25 @@ public class EmployeeHttpController {
 
     @GetMapping("/employees/{name}")
     public ResponseEntity<Employee> byName(@PathVariable("name") String name) {
-        Employee employee = this.service.byName(name);
-        if (employee == null) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        try {
+            Assert.state(Character.isUpperCase(name.charAt(0)), "The name must start with a capital letter");
+            Employee employee = this.service.byName(name);
+            if (employee == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(employee, HttpStatus.OK);
+        } catch (IllegalStateException ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error occurred while processing the request", ex);
         }
-        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+}
+
+@ControllerAdvice
+class ErrorHandlingControllerAdvice {
+    @ExceptionHandler
+    ProblemDetail handleIllegalStateException(IllegalStateException exception) {
+        var pd = ProblemDetail.forStatus(HttpStatusCode.valueOf(500));
+        pd.setDetail("First letter needs to be caps");
+        return pd;
     }
 }
